a8c483fbf6ff35e1cdc0392743f4b28cb6ced615
Khatim <kahtim.dar@gmail.com>
2018-11-24 16:26:53 +0400

    Added managers in the game :P

---
+++ Dishonest/Assets/Individual Sections/Khatim/Scripts/Managers/AudioManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance = null;
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != null)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+    }
+	
+}
