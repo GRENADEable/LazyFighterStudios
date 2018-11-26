ea12dc87b5d0376453f7d88b9e3acd8ae2437e56
Khatim <kahtim.dar@gmail.com>
2018-11-26 16:53:47 +0400

    Set up level grey boxing. Setup basic player controls

---
+++ Dishonest/Assets/Individual Sections/Khatim/Scripts/PlayerControlTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControlTest : MonoBehaviour
+{
+    public float speed;
+    public float rotateSpeed;
+    private CharacterController charController;
+
+    void Start()
+    {
+        charController = GetComponent<CharacterController>();
+    }
+
+    void FixedUpdate()
+    {
+        transform.Rotate(0, Input.GetAxis("Horizontal") * rotateSpeed, 0);
+
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        float curSpeed = speed * Input.GetAxis("Vertical");
+        charController.SimpleMove(forward * curSpeed);
+    }
+}
