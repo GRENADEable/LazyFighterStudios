a17593867bb1f701e4936868166043f60e7608a1
Khatim <kahtim.dar@gmail.com>
2018-12-04 10:54:39 +0400

    Set up the main menu scene, game over scene. Set up the basic menu layout.

--- Dishonest/Assets/Individual Sections/Khatim/Scripts/Managers/UIManager.cs
+++ Dishonest/Assets/Individual Sections/Khatim/Scripts/Managers/UIManager.cs
@@ -1,11 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 public class UIManager : MonoBehaviour
 {
+    public GameObject mainMenuPanel;
+    public GameObject settingsPanel;
+    public GameObject aboutPanel;
+
     void Awake()
     {
+        mainMenuPanel.SetActive(true);
+        settingsPanel.SetActive(false);
+        aboutPanel.SetActive(false);
+    }
 
+    public void NewGame()
+    {
+        SceneManager.LoadScene("DishonestGameScene");
+    }
+
+    public void AboutToMainMenu()
+    {
+        aboutPanel.SetActive(false);
+        mainMenuPanel.SetActive(true);
+    }
+
+    public void SettingsToMainMenu()
+    {
+        settingsPanel.SetActive(false);
+        mainMenuPanel.SetActive(true);
+    }
+
+    public void Quit()
+    {
+        Debug.LogWarning("Quit");
+        Application.Quit();
+    }
+
+    public void SettingsPanel()
+    {
+        mainMenuPanel.SetActive(false);
+        settingsPanel.SetActive(true);
+    }
+
+    public void AboutPanel()
+    {
+        mainMenuPanel.SetActive(false);
+        aboutPanel.SetActive(true);
     }
 }
