9bfdfc708d5ff16c4621fa40afa00f9b5df6d1ae
Khatim <kahtim.dar@gmail.com>
2018-12-17 17:42:52 +0400

    Character controller finished.

--- Dishonest/Assets/Individual Sections/Anna/Scripts/PlayerController.cs
+++ Dishonest/Assets/Individual Sections/Anna/Scripts/PlayerController.cs
@@ -4,38 +4,112 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    Rigidbody playerRB;
-
+    [Header("Player Movement")]
     public float speed;
-    float dragForce = 5;
+    public float runningSpeed;
+    public float gravity;
+    [Header("Camera Clamp")]
+    public float minVerticalClamp;
+    public float maxVerticalClamp;
+    [Header("References")]
+    public GameObject playerReference;
+    public Transform cameraReference;
+    public float cameraRotateSpeed;
+
+    private Vector3 moveDirection = Vector3.zero;
+    private CharacterController controller;
+    private float vertical;
+    [SerializeField]
+    private Collider col;
 
     void Start()
     {
-        playerRB = GetComponent<Rigidbody>();
-        playerRB.drag = dragForce;
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        if (col != null && Input.GetKeyDown(KeyCode.E) && col.gameObject.tag == "Pickup")
+        {
+            col.gameObject.SetActive(false);
+            col = null;
+        }
+
+        if (col != null && Input.GetKeyDown(KeyCode.E) && col.gameObject.tag == "Interact")
+        {
+            this.transform.position = new Vector3(1.0f, 6.0f);
+        }
+
+        if (col != null && Input.GetKeyDown(KeyCode.E) && col.gameObject.tag == "Interact2")
+        {
+            this.transform.position = new Vector3(1.0f, 2.6f);
+        }
     }
 
     void FixedUpdate()
     {
-        /*if (Input.GetKey(KeyCode.W))
+        moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+        moveDirection = transform.TransformDirection(moveDirection);
+        // moveDirection = moveDirection * speed;
+
+        moveDirection.y = moveDirection.y - gravity;
+        // controller.Move(moveDirection);
+
+        if (Input.GetKey(KeyCode.LeftShift))
+            moveDirection = moveDirection * runningSpeed;
+        else
+            moveDirection = moveDirection * speed;
+
+        controller.Move(moveDirection);
+    }
+
+    void LateUpdate()
+    {
+        float horizontal = Input.GetAxis("Mouse X") * cameraRotateSpeed; //Sets the horizonal vaule as the mouse horizontal movement and * by the speed of rotation
+        vertical += Input.GetAxis("Mouse Y") * cameraRotateSpeed; //Sets the horizonal vaule as the mouse horizontal movement and * by the speed of rotation
+
+        //-----------------------------------------------  Setting rotation and movement --------------------------------------
+        playerReference.transform.Rotate(0, horizontal, 0); //Rotates based on the horizontal value assigned.
+        vertical = Mathf.Clamp(vertical, minVerticalClamp, maxVerticalClamp);
+
+        cameraReference.transform.eulerAngles = new Vector3(-vertical, cameraReference.transform.eulerAngles.y, cameraReference.transform.eulerAngles.z);
+    }
+
+    //If player enters a collider, reference the other collider
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Pickup")
         {
-            playerRB.AddForce(transform.forward * speed);
+            col = other;
         }
 
-        if (Input.GetKey(KeyCode.S))
+        if (other.tag == "Interact")
         {
-            playerRB.AddForce(-transform.forward * speed);
+            col = other;
         }
 
-        if (Input.GetKey(KeyCode.A))
+        if (other.tag == "Interact2")
         {
-            playerRB.AddForce(-transform.right * speed);
+            col = other;
+        }
+    }
+
+    //If player leaves a collider, remove the reference
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Pickup")
+        {
+            col = null;
         }
 
-        if (Input.GetKey(KeyCode.D))
+        if (other.tag == "Interact")
         {
-            playerRB.AddForce(transform.right * speed);
+            col = null;
         }
-        //Implement get AXIS for the player movement.*/
+
+        if (other.tag == "Interact2")
+        {
+            col = null;
+        }
     }
 }
